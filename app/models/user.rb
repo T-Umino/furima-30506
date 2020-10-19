b60746f64742b963bfc9acca73e3e90c15f29770
T-Umino <u.r.takaki@gmail.com>
2020-10-19 14:48:36 +0900

    バリデーションの修正・ビューファイルの生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,21 @@ class User < ApplicationRecord
   has_many :purchases
 
   validates :nickname, presence: true
-  validates :email, unique: true
+  validates :email, uniqueness: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers'
 
-  with_options presense: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Last name Full-width characters' } do
     validates :last_name
   end
-  with_options presense: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'First name Full-width characters' } do
     validates :first_name
   end
-  with_options presense: true, format: { with: /\A[ァ-ン]+\z/, message: 'Last name kana Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Last name kana Full-width katakana characters' } do
     validates :last_name_kana
   end
-  with_options presense: true, format: { with: /\A[ァ-ン]+\z/, 'First name kana Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'First name kana Full-width katakana characters' } do
     validates :first_name_kana
   end
   validates :birth_day, presence: true
