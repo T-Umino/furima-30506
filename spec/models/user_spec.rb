80627b17bbb503701ed167a14ac9b578d66f827a
T-Umino <u.r.takaki@gmail.com>
2020-10-20 16:26:33 +0900

    バリデーションの修正・テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,17 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it 'すでに登録されているemailでは登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'emailに「＠」がなければ登録できない'do
+        @user.email = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
