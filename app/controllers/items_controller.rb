924ba7482e42ea7d52faee07de27e7516c1761d6
T-Umino <u.r.takaki@gmail.com>
2020-10-23 15:29:18 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-
+  before_action :set_item, only: [:show]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,9 +20,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :duration_id, :shipping_fee_id, :prefecture_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
