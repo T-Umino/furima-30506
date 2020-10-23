83834c573ecbd3afa082da37be8acfd97887872a
T-Umino <u.r.takaki@gmail.com>
2020-10-23 20:09:20 +0900

    if文を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(item_params)
+    if user_signed_in? && current_user.id === @item.user_id
+      @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit
