c44acfbefbcee2a17818e4927712dc7f0f63cbab
T-Umino <u.r.takaki@gmail.com>
2020-10-27 16:36:07 +0900

    アクションの定義・ビューファイルの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,31 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_root, only: :index
+
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to item_path(@item.id)
+    else
+      render :action :index
+    end
+  end
+
+  private
+
+  def move_to_root
+    if user_signed_in? && current_user.id === @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture, :municipality, :details, :building_name, :tel).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
