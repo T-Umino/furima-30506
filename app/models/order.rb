480fd3a3945485d1b63b914e00ee6e2b9c7f66ac
T-Umino <u.r.takaki@gmail.com>
2020-10-29 11:10:43 +0900

    バリデーションエラーの表示順変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,11 +3,11 @@ class Order
   attr_accessor :postal_code, :prefecture_id, :municipality, :building_name, :details, :tel, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :municipality
-    validates :details
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-).' }
     validates :prefecture_id, numericality: { other_than: 0, message: 'select' }
+    validates :municipality
+    validates :details
     validates :tel, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width numbers.' }
     validates :tel, length: { maximum: 11 }
   end
