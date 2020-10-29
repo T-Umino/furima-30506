510278176bd77cb7730dba2fe645d1f215077ef6
T-Umino <u.r.takaki@gmail.com>
2020-10-29 18:58:51 +0900

    編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if user_signed_in? && current_user.id === @item.user_id
-      @item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit
